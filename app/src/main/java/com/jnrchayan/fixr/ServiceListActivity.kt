82f0c4dbf380a3ec6c97833a5f0548b0336e6636
jnrchayan <jogendranathroy40@gmail.com>
2025-05-05 09:15:50 +0600

    Update

--- app/src/main/java/com/jnrchayan/fixr/ServiceListActivity.kt
+++ app/src/main/java/com/jnrchayan/fixr/ServiceListActivity.kt
@@ -1,6 +1,9 @@
 package com.jnrchayan.fixr
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,7 +14,9 @@ class ServiceListActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var serviceList: ArrayList<ServiceModel>
+    private lateinit var filteredList: ArrayList<ServiceModel>
     private lateinit var adapter: ServiceAdapter
+    private lateinit var etSearch: EditText
     private val databaseRef = FirebaseDatabase.getInstance().getReference("servicelist")
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +25,23 @@ class ServiceListActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.recyclerViewServices)
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        etSearch = findViewById(R.id.editTextSearch)
+
         serviceList = ArrayList()
-        adapter = ServiceAdapter(serviceList)
+        filteredList = ArrayList()
+        adapter = ServiceAdapter(filteredList)
         recyclerView.adapter = adapter
+
+        etSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                filterServices(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
 
         loadServices()
     }
@@ -35,12 +54,26 @@ class ServiceListActivity : AppCompatActivity() {
                     val service = serviceSnap.getValue(ServiceModel::class.java)
                     service?.let { serviceList.add(it) }
                 }
-                adapter.notifyDataSetChanged()
+                filterServices(etSearch.text.toString())
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Toast.makeText(this@ServiceListActivity, "Failed to load services", Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    private fun filterServices(query: String) {
+        val lowerCaseQuery = query.lowercase()
+        filteredList.clear()
+        for (service in serviceList) {
+            if (service.title.lowercase().contains(lowerCaseQuery) ||
+                service.category.lowercase().contains(lowerCaseQuery) ||
+                service.location.lowercase().contains(lowerCaseQuery)
+            ) {
+                filteredList.add(service)
+            }
+        }
+        adapter.notifyDataSetChanged()
     }
 }
